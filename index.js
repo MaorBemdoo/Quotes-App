79ba29221bb8a4d00fe2463e6712ddf9b8d96085
Bemdoo Maor <bemdoo.maor@gmail.com>
2023-08-28 16:35:40 +0100

    Restuctured index.js to test the fetch api and params

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 
-const params = {
-  name: "Bemdoo Maor",
-  email: "bemdoo.maor1@gmail.com,",
-}
-
-fetch("https://type.fit/api/quotes")
+fetch("https://api.quotable.io/quotes/random")
       .then(response => {
         return response.json();
       })
       .then(data => {
         // console.log(data);
-        console.log(data[0].text)
-        params.message = data[0].text
+        console.log(data[0].content)
+        document.querySelector("main p").innerHTML = data[0].content
+        // params.message = data[0].content
+        const params = {
+          name: "Bemdoo Maor",
+          email: "bemdoo.maor1@gmail.com,",
+          // message: document.querySelector("main p").getInnerHTML()
+          message: data[0].content
+        }
+        console.log(params.message);
+
+        const serviceID = "service_u50j5ho";
+        const templateID = "template_397imxp";
+
+        emailjs
+          .send(serviceID, templateID, params)
+          .then(res => {
+            console.log(res);
+            alert("Mail sent successfully🎉🎉🥳")
+          })
+          .catch(err => {
+            alert(err)
+          })
       })
       .catch(err => {
         console.log(err);
       })
-
-// console.log(params.message);
-
-const serviceID = "service_u50j5ho";
-const templateID = "template_397imxp";
-
-emailjs
-  .send(serviceID, templateID, params)
-  .then(res => {
-    console.log(res);
-    alert("Mail sent successfully🎉🎉🥳")
-  })
-  .catch(err => {
-    alert(err)
-  })
