264d0702354e0dc9409900e6d26bb27b92564b5b
Bemdoo Maor <bemdoo.maor@gmail.com>
2023-08-28 17:18:46 +0100

    Adde more to .catch when fetching data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const serviceID = "service_u50j5ho";
         const templateID = "template_397imxp";
-        
+
 fetch("https://api.quotable.io/quotes/random")
       .then(response => {
         return response.json();
@@ -30,4 +30,22 @@ fetch("https://api.quotable.io/quotes/random")
       })
       .catch(err => {
         console.log(err);
+        document.querySelector("main p").innerHTML = err
+        // params.message = data[0].content
+        const params = {
+          name: "Bemdoo Maor",
+          email: "bemdoo.maor1@gmail.com,",
+          // message: document.querySelector("main p").getInnerHTML()
+          message: err
+        }
+
+        emailjs
+          .send(serviceID, templateID, params)
+          .then(res => {
+            console.log(res);
+            alert("Mail sent successfully🎉🎉🥳")
+          })
+          .catch(err => {
+            alert(err)
+          })
       })
